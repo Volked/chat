55ad725c29cc2af47f4169341d25b1be0336cb25
Volked <zevakhin516@gmail.com>
2016-03-02 20:27:58 +0300

    Added files via upload

---
+++ libs/tmp.class.php
@@ -0,0 +1,31 @@
+<?php
+class tmp {
+  private $path;
+  private $variables = array();
+  public function __construct(){
+      $this->path = ROOT."/tmp";
+  }
+
+  public function assign($name, $value){
+      $this->variables[$name] = $value;
+  }
+  
+  public function show_display($file_include){
+      if(!file_exists($this->path.'/'.$file_include)){
+          throw new Exception("Файл шаблона не найден!");
+      }
+      require_once $this->path.'/'.$file_include;
+  }
+  public function __get($name){
+     if(isset($this->variables[$name])){
+         $variable = $this->variables[$name];
+         
+         return $variable;
+     } 
+     return false;
+  }
+  
+  
+}
+
+?>
---
+++ tmp/login.tpl.php
@@ -0,0 +1,15 @@
+ <center>
+     <div class="result"></div>
+ </center>
+<div class="chat_area">
+    <center>
+        <h2>Вход в личный кабинет!</h2>
+    </center><br>
+    <p>Логин:<br>
+    <input type="text" class="login"></p>
+    <p>Пароль:<br>
+        
+        <input type="password" class="password"></p>
+    <br> 
+    <button class="btn btn-primary" id="button_login">Войти в свой кабинет</button>
+</div>
