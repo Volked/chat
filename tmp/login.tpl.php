f2dbe16449c541e68f1e6047cbfdd0949200fb13
Andrew <zevakhin516@gmail.com>
2016-03-14 23:21:44 +0300

    register

--- tmp/login.tpl.php
+++ tmp/login.tpl.php
@@ -4,35 +4,35 @@
 </center>
 <div class="chat_area">
     <center>
-        <h2>Вход в личный кабинет!</h2>
+        <h2>Р’С…РѕРґ РІ Р»РёС‡РЅС‹Р№ РєР°Р±РёРЅРµС‚!</h2>
     </center>
     <br>
 
-    <p>Логин:<br>
+    <p>Р›РѕРіРёРЅ:<br>
         <input type="text" class="login"></p>
 
-    <p>Пароль:<br>
+    <p>РџР°СЂРѕР»СЊ:<br>
 
         <input type="password" class="password"></p>
     <br>
-    <button class="btn btn-primary" id="button_login">Войти в свой кабинет</button><br>
-	<center><a>У вас еще нету аккаунта?Так зарегистрируйтесь</a>
+    <button class="btn btn-primary" id="button_login">Р’РѕР№С‚Рё РІ СЃРІРѕР№ РєР°Р±РёРЅРµС‚</button><br>
+	<center><a>РЈ РІР°СЃ РµС‰Рµ РЅРµС‚Сѓ Р°РєРєР°СѓРЅС‚Р°?РўР°Рє Р·Р°СЂРµРіРёСЃС‚СЂРёСЂСѓР№С‚РµСЃСЊ</a>
 <form action="register.php" method="POST" />
     <table>
         <tr>
-            <td>Введите логин:</td>
+            <td>Р’РІРµРґРёС‚Рµ Р»РѕРіРёРЅ:</td>
             <td><input type="text" size="20" name="login" /></td>
         </tr>
         <tr>
-            <td>Введите пароль:</td>
+            <td>Р’РІРµРґРёС‚Рµ РїР°СЂРѕР»СЊ:</td>
             <td><input type="password" size="20" maxlength="20" name="password" /></td>
         </tr>
         <tr>
 		 <tr>
-            <td>Введите цвет:</td>
+            <td>Р’РІРµРґРёС‚Рµ С†РІРµС‚:</td>
             <td><input type="text" size="20" name="color" /></td>
         </tr>
-            <td colspan="2"><input class="btn btn-primary" id="button_login" type="submit" value="Зарегистрироваться" name="submit" /></td>
+            <td colspan="2"><input class="btn btn-primary" id="button_login" type="submit" value="Р—Р°СЂРµРіРёСЃС‚СЂРёСЂРѕРІР°С‚СЊСЃСЏ" name="submit" /></td>
         </tr>
     </table>
 </form>
