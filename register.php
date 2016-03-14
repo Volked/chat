77327c0181d9ffb01b71189c8f705a4f9d4641da
Andrew <zevakhin516@gmail.com>
2016-03-14 23:50:25 +0300

    Исправлены баги

--- register.php
+++ register.php
@@ -5,11 +5,11 @@ if (isset($_POST['submit']))
 {
     if (empty($_POST['login'])) 
     {
-        $info_reg = 'Вы не ввели Логин';
+        $info_reg = 'Р’С‹ РЅРµ РІРІРµР»Рё Р›РѕРіРёРЅ';
     }                 
     elseif (empty($_POST['password'])) 
     {
-        $info_reg = 'Вы не ввели пароль';
+        $info_reg = 'Р’С‹ РЅРµ РІРІРµР»Рё РїР°СЂРѕР»СЊ';
     }                      
    else 
     {
@@ -24,7 +24,7 @@ if (isset($_POST['submit']))
       ";
       $sql = mysqli_query($db, $query) or die(mysql_error());
 	  if (mysqli_num_rows($sql) > 0){
-		  echo 'Аккаунт:'. $_POST['login'] ."\nуже существует!Используйте другое имя пользователя";
+		  echo 'РђРєРєР°СѓРЅС‚:'. $_POST['login'] ."\nСѓР¶Рµ СЃСѓС‰РµСЃС‚РІСѓРµС‚!РСЃРїРѕР»СЊР·СѓР№С‚Рµ РґСЂСѓРіРѕРµ РёРјСЏ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ";
 	  }
 	  else
 	  {
@@ -32,7 +32,7 @@ if (isset($_POST['submit']))
         VALUES ('$login', '$color', '$password')";
         $result = mysqli_query($db, $query) or die(mysql_error());
                     
-        $info_reg = 'Вы успешно зарегистрировались!';
+        $info_reg = 'Р’С‹ СѓСЃРїРµС€РЅРѕ Р·Р°СЂРµРіРёСЃС‚СЂРёСЂРѕРІР°Р»РёСЃСЊ!';
 	  }
 }
 }
